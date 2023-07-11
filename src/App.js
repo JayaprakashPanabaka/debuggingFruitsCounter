d75f5ce20dc773fc3f778d412d489543f58a1675
JayaprakashPanabaka <jpanabaka1729@gmail.com>
2023-07-11 09:03:03 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import FruitsCounter from './components/FruitsCounter/index'
+
+import './App.css'
+
+const App = () => <FruitsCounter />
+
+// const App = () => <div>Hii Darling</div>
+
+export default App
